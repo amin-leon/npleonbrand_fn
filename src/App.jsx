34cfca63c0bd6dea8766c08a0591b3b6ebc9db45
NPL Codes <104311071+amin-leon@users.noreply.github.com>
2025-04-10 16:58:30 +0200

    fix url

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,46 @@
 import { useState } from "react";
 import "./App.css";
-import { Routes, Route, BrowserRouter as Router } from "react-router";
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+
 import ErrorPage from "./components/common/ErrorPage";
 import Layout from "./components/layout/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import BlogDetails from "./pages/BlogDetails"
+import BlogDetails from "./pages/BlogDetails";
 import Blogs from "./pages/Blogs";
 import Skills from "./pages/Skills";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
-import { Education, Experience, Training } from "./components/sections/Education";
-
+import {
+  Education,
+  Experience,
+  Training,
+} from "./components/sections/Education";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />}>
-              <Route index element={<Education />} />
-              <Route path="experience" element={<Experience />} />
-              <Route path="training" element={<Training />} />
-            </Route>
-            <Route path="/blogs" element={<Blogs />} />
-            <Route path="/blogs/:id" element={<BlogDetails />} />
-            <Route path="/skills" element={<Skills />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
+    <Router basename="/npleonbrand_fn">
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />}>
+            <Route index element={<Education />} />
+            <Route path="experience" element={<Experience />} />
+            <Route path="training" element={<Training />} />
           </Route>
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/blogs/:id" element={<BlogDetails />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
 
-          {/* Error page should not use Layout */}
-          <Route path="*" element={<ErrorPage />} />
-        </Routes>
-      </Router>
-    </>
+        {/* Fallback route for 404 */}
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
   );
 }
 
--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,5 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  base:'/npleonbrand_fn'
+  base:'/npleonbrand_fn/'
 })
